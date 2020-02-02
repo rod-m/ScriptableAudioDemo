1a0562aeca9c6e749c8945a0909e85bed3f6d2b5
rod martin <rod@araknia.com>
2020-02-02 16:30:20 +0000

    Initial commit

---
+++ Assets/Audio_Scriptable_Object/Audio/SimpleAudioEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+//To add multiple Simple Audio Event assets use Assets/Audio/Simple
+[CreateAssetMenu(menuName="Audio Events/Simple")]
+public class SimpleAudioEvent : AudioEvent
+{
+    public AudioClip[] clips;
+
+    public RangedFloat volume;
+
+    [MinMaxRange(0, 2)]
+    public RangedFloat pitch;
+
+    public override void Play(AudioSource source, float _velocity)
+    {
+        if (clips.Length == 0) return;
+        
+        source.clip = clips[Random.Range(0, clips.Length)];
+        source.volume = _velocity + Random.Range(volume.minValue, volume.maxValue);
+        source.pitch = _velocity + Random.Range(pitch.minValue, pitch.maxValue);
+        Debug.Log(String.Format("Input velocity {0} Volume {1} Pitch {2}", _velocity, source.volume, source.pitch));
+        source.Play();
+    }
+}
---
+++ Assets/Scripts/CrashingBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(Rigidbody))]
+public class CrashingBox : MonoBehaviour
+{
+    private Rigidbody rb;
+    private AudioSource audioSrc;
+    public SimpleAudioEvent crashSFX;
+   
+   
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Vector3 velocity = rb.velocity;
+       
+        
+        if (crashSFX != null)
+        {
+            crashSFX.Play(audioSrc, velocity.magnitude);
+        }
+      
+    }
+}
